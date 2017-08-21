59a6786d916ae35f83d5cfed4bf91ab0e7353177
Jagori Samajdar <jagori.ganguli@gmail.com>
2017-08-21 18:51:47 +0530

    Update Door.cs to remove the echo sound when door opens

--- P3 Maze/Assets/Udacity/Scripts/Door.cs
+++ P3 Maze/Assets/Udacity/Scripts/Door.cs
@@ -8,7 +8,6 @@ public class Door : MonoBehaviour
 	private bool locked = true;
     // Create a boolean value called "opening" that can be checked in Update() 
 	private bool opening=false;
-
 	public AudioClip[] audioClips;
 	private AudioSource audioSource;
 
@@ -17,8 +16,7 @@ public class Door : MonoBehaviour
             // Animate the door raising up
 		if(opening){
 			transform.Translate (0, 2.5f * Time.deltaTime, 0, Space.World);
-			audioSource = GetComponent<AudioSource> ();
-			audioSource.PlayOneShot (audioClips[1]);
+
 		}
     }
 
@@ -28,6 +26,8 @@ public class Door : MonoBehaviour
 		// Set the "opening" boolean to true
 		if (locked==false) {
 			opening = true;
+			audioSource = GetComponent<AudioSource> ();
+			audioSource.PlayOneShot (audioClips[1]);
 
 		}
 		else {
